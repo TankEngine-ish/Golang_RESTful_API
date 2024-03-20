13cbb92a7b47cbffd754d479a960dcb98a416172
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2024-03-21 00:40:41 +0200

    create first endpoint

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func homepage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+	fmt.Println("Endpoint Hit: homepage")
+}
+
+func main() {
+	http.HandleFunc("/", homepage)
+	http.ListenAndServe("localhost:10000", nil) // nil is the handler
 }
