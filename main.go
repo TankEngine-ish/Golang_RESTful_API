89d53b2fb8d615ee61af6df0f20b3183d626ad9e
TankEngine-ish <plamendimitrov0308@yahoo.com>
2024-03-22 20:07:53 +0200

    implement mysql functionality[comment out old code]

--- main.go
+++ main.go
@@ -1,60 +1,97 @@
 package main
 
 import (
-	"encoding/json"
+	"database/sql"
 	"fmt"
 	"log"
-	"net/http"
 
-	"github.com/gorilla/mux"
+	_ "github.com/go-sql-driver/mysql" // _ means we are not using this package directly
 )
 
-type Product struct {
-	Id       string
-	Name     string
-	Quantity int
-	Price    float64
-}
-
-var Products []Product
-
-func homepage(w http.ResponseWriter, r *http.Request) {
-	log.Println("Endpoint Hit: homepage")
-	fmt.Fprintf(w, "Hello, World!")
-}
-
-func returnAllProducts(w http.ResponseWriter, r *http.Request) {
-	log.Println("Endpoint Hit: returnAllProducts")
-	json.NewEncoder(w).Encode(Products)
-}
-
-func getProduct(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	key := vars["id"]
-	for _, product := range Products {
-		if string(product.Id) == key {
-			json.NewEncoder(w).Encode(product)
-		}
+func checkError(e error) {
+	if e != nil {
+		log.Fatalln(e)
 	}
 }
 
-func handleRequests() {
-	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/products", returnAllProducts)
-	myRouter.HandleFunc("/product/{id}", getProduct)
-	myRouter.HandleFunc("/", homepage)
-	http.ListenAndServe(":10000", myRouter)
+type Data struct {
+	id   int
+	name string
 }
 
 func main() {
+	connectionString := fmt.Sprintf("%v:%v@tcp(127.0.0.1:3306)/%v", DBUser, DBPassword, DBName)
+	db, err := sql.Open("mysql", connectionString)
+	checkError(err)
+	defer db.Close()
 
-	Products = []Product{
-		{Id: "1", Name: "Laptop", Quantity: 10, Price: 1000.00},
-		{Id: "2", Name: "Headphones", Quantity: 50, Price: 10.00},
+	rows, err := db.Query("SELECT * FROM data")
+	checkError(err)
+
+	for rows.Next() {
+		var data Data
+		err = rows.Scan(&data.id, &data.name)
+		checkError(err)
+		fmt.Println(data)
+
 	}
-
-	handleRequests()
-
-	http.HandleFunc("/", homepage)
-	http.ListenAndServe("localhost:10000", nil) // nil is the handler
 }
+
+// import (
+// 	"encoding/json"
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+
+// 	"github.com/gorilla/mux"
+// )
+
+// type Product struct {
+// 	Id       string
+// 	Name     string
+// 	Quantity int
+// 	Price    float64
+// }
+
+// var Products []Product
+
+// func homepage(w http.ResponseWriter, r *http.Request) {
+// 	log.Println("Endpoint Hit: homepage")
+// 	fmt.Fprintf(w, "Hello, World!")
+// }
+
+// func returnAllProducts(w http.ResponseWriter, r *http.Request) {
+// 	log.Println("Endpoint Hit: returnAllProducts")
+// 	json.NewEncoder(w).Encode(Products)
+// }
+
+// func getProduct(w http.ResponseWriter, r *http.Request) {
+// 	vars := mux.Vars(r)
+// 	key := vars["id"]
+// 	for _, product := range Products {
+// 		if string(product.Id) == key {
+// 			json.NewEncoder(w).Encode(product)
+// 		}
+// 	}
+// }
+
+// func handleRequests() {
+// 	myRouter := mux.NewRouter().StrictSlash(true)
+// 	myRouter.HandleFunc("/products", returnAllProducts)
+// 	myRouter.HandleFunc("/product/{id}", getProduct)
+// 	myRouter.HandleFunc("/", homepage)
+// 	http.ListenAndServe(":10000", myRouter)
+// }
+
+// func main() {
+
+// 	Products = []Product{
+// 		{Id: "1", Name: "Laptop", Quantity: 10, Price: 1000.00},
+// 		{Id: "2", Name: "Headphones", Quantity: 50, Price: 10.00},
+// 	}
+
+// 	handleRequests()
+
+// 	http.HandleFunc("/", homepage)
+// 	http.ListenAndServe("localhost:10000", nil) // nil is the handler
+// }
