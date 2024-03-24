7def212d0a042b77f67d004c638ac2c0d987339c
TankEngine-ish <plamendimitrov0308@yahoo.com>
2024-03-25 01:07:46 +0200

    add product query

--- model.go
+++ model.go
@@ -1,6 +1,9 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+)
 
 type product struct {
 	ID       int     `json:"id"`
@@ -28,4 +31,15 @@ func getProducts(db *sql.DB) ([]product, error) {
 
 	}
 	return products, nil
+}
+
+func (p *product) getProduct(db *sql.DB) error {
+	query := fmt.Sprintf("SELECT name, quantity, price FROM products where id=%v", p.ID)
+	row := db.QueryRow(query)
+	err := row.Scan(&p.Name, &p.Quantity, &p.Price)
+	if err != nil {
+		return err
+	}
+	return nil
+
 }
