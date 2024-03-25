cb3486d3c1ad7d9a1a786f1775d959b60e41444a
TankEngine-ish <plamendimitrov0308@yahoo.com>
2024-03-25 02:18:32 +0200

    POST route finished

--- app.go
+++ app.go
@@ -74,9 +74,27 @@ func (app *App) getProduct(w http.ResponseWriter, r *http.Request) {
 	}
 	sendResponse(w, http.StatusOK, p)
 }
+
+func (app *App) createProduct(w http.ResponseWriter, r *http.Request) {
+	var p product
+	err := json.NewDecoder(r.Body).Decode(&p)
+	if err != nil {
+		sendError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	err = p.createProduct(app.DB)
+	if err != nil {
+		sendError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	sendResponse(w, http.StatusOK, p)
+
+}
+
 func (app *App) handleRoutes() {
 	app.Router.HandleFunc("/products", app.getProducts).Methods("GET")
 	app.Router.HandleFunc("/product/{id}", app.getProduct).Methods("GET")
+	app.Router.HandleFunc("/product", app.createProduct).Methods("POST")
 }
 
 // ******** OLD CODE ********
