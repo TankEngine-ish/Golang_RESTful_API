08bbb97a368fb164e678fe8821ecce9cd2ff7639
TankEngine-ish <plamendimitrov0308@yahoo.com>
2024-03-25 16:00:23 +0200

    finish GET testing method + create test database

--- app_test.go
+++ app_test.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -12,11 +15,61 @@ func TestMain(m *testing.M) {
 	if err != nil {
 		log.Fatal("An error occurred while initializing the database")
 	}
-
+	createTable()
 	m.Run()
 
 }
 
 func createTable() {
+	createTableQuery := `CREATE TABLE IF NOT EXISTS products(
+		id int NOT NULL AUTO_INCREMENT,
+		name varchar(255) NOT NULL,
+		quantity int,
+		price float(10,7),
+		PRIMARY KEY (id)
+	);`
+	_, err := a.DB.Exec(createTableQuery)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func clearTable() {
+	a.DB.Exec("DELETE FROM products")
+	a.DB.Exec("ALTER TABLE products AUTO_INCREMENT = 1")
+	log.Println("clearTable")
+}
+
+func addProduct(name string, quantity int, price float64) {
+	query := fmt.Sprintf("INSERT INTO products(name, quantity, price) VALUES('%v', %v, %v)", name, quantity, price)
+	_, err := a.DB.Exec(query)
+	if err != nil {
+		log.Println(err)
+
+	}
+
+	a.DB.Exec(query)
+}
+
+func TestGetProduct(t *testing.T) {
+	// Clear the table
+	clearTable()
+	addProduct("keyboard", 100, 140)
+	request, _ := http.NewRequest("GET", "/product/1", nil)
+	response := sendRequest(request)
+	checkStatusCode(t, http.StatusOK, response.Code)
+
+}
+
+func checkStatusCode(t *testing.T, expectedStatusCode int, actualStatusCode int) {
+	if expectedStatusCode != actualStatusCode {
+		t.Errorf("Expected status code %v but got %v", expectedStatusCode, actualStatusCode)
+	}
+}
+
+func sendRequest(request *http.Request) *httptest.ResponseRecorder {
+	recorder := httptest.NewRecorder()
+	a.Router.ServeHTTP(recorder, request)
+	return recorder
 
 }
